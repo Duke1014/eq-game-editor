c5d5806c5ebd626eb1f2288e1865d42435f90c28
Duke Norsworthy <duke.norsworthy@gmail.com>
2025-07-13 23:13:12 -0400

    npc editor initiated, list of editors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NPCEditor from "./pages/NPCEditor";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/npcs" element={<NPCEditor />} />
     </Routes>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom";
+import NPCEditor from "./NPCEditor";
+
 export default function Home() {
   return (
     <div>
-        Hello
+        Hello welcome Home
+        <div className="list-of-editors">
+            Editors:
+            <ul>
+                <li>
+                    <Link to="/npcs" element={<NPCEditor />}>NPC</Link>
+                </li>
+                <li>
+                    Item 
+                </li>
+                <li>
+                    Spell 
+                </li>
+                <li>
+                    Tradeskills 
+                </li>
+                <li>
+                    thing
+                </li>
+            </ul>
+        </div>
     </div>
   )
 }
